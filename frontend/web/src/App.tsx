29a8142b14b860f4ee90b0fb6b6ecc0028dbc31a
yukimingo <hakamori5junk.1@gmail.com>
2025-05-18 15:30:17 +0900

    ルーティング定義

--- frontend/web/src/App.tsx
+++ frontend/web/src/App.tsx
@@ -1,12 +1,46 @@
-import "./App.css";
+// import React from "react";
+// import type { ReactNode } from "react";
+// import "./App.css";
+
+// type Props = {
+//   children: ReactNode;
+// };
+
+// const Layout: React.FC<Props> = ({ children }) => {
+//   return (
+//     <div className="app">
+//       <header className="app-header">
+//         <h1>My Stylish App</h1>
+//         <nav>
+//           <a href="#">Home</a>
+//           <a href="#">Users</a>
+//           <a href="#">About</a>
+//         </nav>
+//       </header>
+
+//       <main className="app-main">{children}</main>
+
+//       <footer className="app-footer">
+//         <p>© {new Date().getFullYear()} My Stylish App. All rights reserved.</p>
+//       </footer>
+//     </div>
+//   );
+// };
+
+// export default Layout;
+
+import { Routes, Route } from "react-router-dom";
+import Layout from "./layout";
+import UserList from "./pages/user";
 
 function App() {
   return (
-    <>
-      <div>
-        <h1>This is React app test.</h1>
-      </div>
-    </>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<UserList />} />
+        <Route path="/user" element={<UserList />} />
+      </Routes>
+    </Layout>
   );
 }
 
